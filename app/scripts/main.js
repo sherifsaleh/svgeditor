ae3d6f0159360c7e446bb5c0eaabcbcc6cdea8fb
SALEH Sherif <sherifsaleh111@gmail.com>
2016-12-27 23:35:09 +0100

    oo method not working with curring

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,10 +15,11 @@
     let svg;
     let modifyTextState;
 
+
     // DOM elements
-    let inputForm   = document.getElementById("text-editor");
-    let input       = document.getElementById('text-editor__input');
-    let svgImg      = document.getElementById('svg-image');
+    let inputForm = document.getElementById("text-editor");
+    let input = document.getElementById('text-editor__input');
+    let svgImg = document.getElementById('svg-image');
 
 
     // load svg element to the page
@@ -30,50 +31,47 @@
         svgObj.parentNode.replaceChild(svg, svgObj);
         svgNodes = svg.childNodes;
 
+        // start the program
+        functionality.init();
+    }, false);
 
-        // attach event to every node and return a function
-        for (let i = 0; i < svgNodes.length; i++) {
-            svgNodes[i].addEventListener("click", function() { svgNodeClicker(i, svgNodes[i] ) }, false);
-        }
-
-
-        // Function to change the content of svgNodes
-        // curring because we are gathering elements from two functions
-        // svgNodes clicked and inputSubmit
-        let modifyText =
-            (svg) =>
-                (elementId) =>
-                    (elementText) =>
-                        svg.childNodes[elementId].innerHTML = elementText;
-        // let modifyText = (i, text) => {
-        //     svg.childNodes[i].innerHTML = text;
-        //     console.log( svg );
-        // }
-
-        //modifyText(svg)(3)('hello');
-
-
-        // function get the clicked node
-        let svgNodeClicker = (i, svgNode ) => {
+    let functionality = {
+        init: () => {
+            // initialize submission
+            functionality.submission();
+            // attach event to every node and return a function
+            for (let i = 0; i < svgNodes.length; i++) {
+                svgNodes[i].addEventListener("click", function() { functionality.svgNodeClicks(i, svgNodes[i]) }, false);
+            }
+        },
+        svgNodeClicks: (i, svgNode) => {
 
             // add class of selected element
             input.value = svgNode.innerHTML;
 
-            // do some logic
-            // show editor
             // on editor submit update node with value
-            modifyTextState = modifyText(svg)(i);
-            // define type of clicked node
-            // call right function if text or graphical element or image
+            modifyTextState = functionality.textEdit(svg)(i);
+
+        },
+        textEdit: (svg, elementId, elementText) => {
+            // Function to change the content of svgNodes
+            // curring because we are gathering elements from two functions
+            // svgNodes clicked and inputSubmit
+            let modifyText =
+                (svg) =>
+                    (elementId) =>
+                        (elementText) => svg.childNodes[elementId].innerHTML = elementText;
+
+            return modifyText;
+        },
+        submission: () => {
+            // listen to form submit
+            inputForm.addEventListener("submit", inputSubmit, false);
+
+            function inputSubmit(evt) {
+                evt.preventDefault();
+                modifyTextState(input.value);
+            }
         }
-
-        // listen to form submit
-        inputForm.addEventListener("submit", inputSubmit, false );
-
-        function inputSubmit( evt ) {
-            evt.preventDefault();
-            modifyTextState( input.value );
-        }
-
-    }, false);
+    }
 })();
