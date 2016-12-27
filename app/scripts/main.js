1d4a9e0269fee43d19bc11b3dcfa2545be6985a4
SALEH Sherif <sherifsaleh111@gmail.com>
2016-12-27 22:14:12 +0100

    modify text function on submit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,69 +8,72 @@
     });
 
 
+    // variables
     let svgNodes;
     let svgObj;
     let svgDoc;
     let svg;
+    let modifyTextState;
 
-    let inputForm = document.getElementById("text-editor");
-    let input = document.getElementById('text-editor__input');
-    let mySVG = document.getElementById('mySVG');
+    // DOM elements
+    let inputForm   = document.getElementById("text-editor");
+    let input       = document.getElementById('text-editor__input');
+    let svgImg      = document.getElementById('svg-image');
 
 
-    // load svg element
+    // load svg element to the page
     // full details : https://css-tricks.com/ajaxing-svg-sprite/
-    mySVG.addEventListener('load', function(e) {
-
+    svgImg.addEventListener('load', function(e) {
         svgObj = e.currentTarget;
         svgDoc = svgObj.getSVGDocument();
         svg = svgDoc.childNodes[0];
-
         svgObj.parentNode.replaceChild(svg, svgObj);
-
-
         svgNodes = svg.childNodes;
-
-        // attach event listener
-        function addEvent(element, event_name, func) {
-            if (element.addEventListener) {
-                element.addEventListener(event_name, func, false);
-            } else if (element.attachEvent) {
-                element.attachEvent("on" + event_name, func);
-            }
-        }
 
 
         // attach event to every node and return a function
         for (let i = 0; i < svgNodes.length; i++) {
-            svgNodes[i].addEventListener("click", function() { svgNodeClicker(i, "hey") }, false);
+            svgNodes[i].addEventListener("click", function() { svgNodeClicker(i, svgNodes[i] ) }, false);
         }
 
 
+        // Function to change the content of svgNodes
+        // curring because we are gathering elements from two functions
+        // svgNodes clicked and inputSubmit
+        let modifyText =
+            (svg) =>
+                (elementId) =>
+                    (elementText) =>
+                        svg.childNodes[elementId].innerHTML = elementText;
+        // let modifyText = (i, text) => {
+        //     svg.childNodes[i].innerHTML = text;
+        //     console.log( svg );
+        // }
+
+        //modifyText(svg)(3)('hello');
+
+
+        // function get the clicked node
+        let svgNodeClicker = (i, svgNode ) => {
+
+            // add class of selected element
+            input.value = svgNode.innerHTML;
+
+            // do some logic
+            // show editor
+            // on editor submit update node with value
+            modifyTextState = modifyText(svg)(i);
+            // define type of clicked node
+            // call right function if text or graphical element or image
+        }
+
+        // listen to form submit
+        inputForm.addEventListener("submit", inputSubmit, false );
+
+        function inputSubmit( evt ) {
+            evt.preventDefault();
+            modifyTextState( input.value );
+        }
+
     }, false);
-
-
-    // function get the clicked node
-    function svgNodeClicker(i, text) {
-        // do some logic
-        // define type of clicked node
-        // call right function if text or graphical element or image
-        //console.log( this.innerHTML, this.id,  i);
-        modifyText(i, text)
-    }
-
-    // Function to change the content of svgNodes
-    function modifyText(i, text) {
-        svg.childNodes[i].innerHTML = text;
-    }
-
-
-    inputForm.addEventListener("submit", inputSubmit);
-
-    function inputSubmit( evt ) {
-        evt.preventDefault();
-        return input.value;
-    }
-
-
 })();
