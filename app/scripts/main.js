79322887a8e9c5192252ba2411d18517aefb992e
SALEH Sherif <sherifsaleh111@gmail.com>
2017-01-03 15:18:54 +0100

    Progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -152,16 +152,18 @@
 
     /***************** check in array *****************/
     let checkInArray = ( arr, fontFamilyStr, fontWeight ) => {
+      // check if fontWeight is not defined
       if( fontWeight == null ){
-        if ( arr.indexOf(fontFamilyStr) === -1 ){
-            arr.push(fontFamilyStr)
-        }
-      }else {
-        if ( arr.indexOf(fontFamilyStr+':'+fontWeight) === -1 ){
-           arr.push(fontFamilyStr+':'+fontWeight)
-        }
+        // check if element isn't in the array push the element
+        // push element without the font-weight option
+        if ( arr.indexOf(fontFamilyStr) === -1 ) arr.push(fontFamilyStr)
       }
-
+      // check if fontWeight is already defined
+      else {
+        // push element with the font-weight option
+        // arr.indexOf(fontFamilyStr+':'+fontWeight) === -1
+        if ( arr.indexOf(fontFamilyStr+':'+fontWeight) === -1 ) arr.push(fontFamilyStr+':'+fontWeight)
+      }
     }
 
 
